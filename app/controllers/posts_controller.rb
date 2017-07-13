3b07b2dbafce5e12eb210fa6563458b42f6c2bf4
Joseph <jvg_0119@yahoo.com>
2017-07-13 11:04:41 -0700

    assign-31-intro-crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
+
+    @posts.each_with_index do |post, index|
+      if (index + 1) % 5 == 0
+        post.title = "spam"
+      # if (post.id % 5 == 0)
+      #   post.title = "spam"
+       end
+    #   raise
+     end
   end
 
   def show
