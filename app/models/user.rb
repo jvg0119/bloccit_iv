8238b63b77b535706c479f430023e97d454eb5d3
Joseph <jvg_0119@yahoo.com>
2017-07-17 15:25:52 -0700

    cp-36-authentication-user-model

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+
+  before_save { self.email = email.downcase if email.present? }
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 100 }
+
+  validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
+  validates :password, length: { minimum: 6 }, allow_blank: true
+
+  validates :email, presence: true, length: { minimum: 3, maximum: 254 },
+            uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  
+end
