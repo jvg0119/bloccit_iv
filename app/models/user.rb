bf18c809e335d8688588aa99a5625c17f50fe95c
Joseph <jvg_0119@yahoo.com>
2017-07-17 18:26:13 -0700

    assign-36-authentication-user-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,23 @@
 class User < ApplicationRecord
 
   before_save { self.email = email.downcase if email.present? }
+  before_save :format_name
 
   validates :name, presence: true, length: { minimum: 1, maximum: 100 }
 
   validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
   validates :password, length: { minimum: 6 }, allow_blank: true
 
+  EMAIL_REGEX = /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/
   validates :email, presence: true, length: { minimum: 3, maximum: 254 },
-            uniqueness: { case_sensitive: false }
+            uniqueness: { case_sensitive: false },
+            format: { with: EMAIL_REGEX }
 
   has_secure_password
-  
+
+
+  def format_name
+    self.name = self.name.split(' ').map{ |n| n.capitalize }.join(' ') if name.present?
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_length_of(:email).is_at_least(3) }
   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
+#  it { is_expected.to validates_format_of(:email) }
 
   # shoulda tests for password
   it { is_expected.to validate_presence_of(:password) }
@@ -21,6 +22,11 @@ RSpec.describe User, type: :model do
   describe "attributes" do
     it "should have a name and email" do
       expect(user).to have_attributes(name: "Blocit Use", email: "user@bloccit.com")
+    end
+    it "should format user's name" do
+      user.name = "joe garcia"
+      user.save
+      expect(user.name).to eq("Joe Garcia")
     end
   end
 
@@ -32,9 +38,21 @@ RSpec.describe User, type: :model do
       expect(user_with_invalid_name).to be_invalid
       expect(user_with_invalid_name).to_not be_valid
     end
+
     it "should be invalid due to a blank email" do
       expect(user_with_invalid_email).to be_invalid
       expect(user_with_invalid_email).to_not be_valid
+    end
+  end
+
+# same test as the attributes should format user's name 
+  describe "format_name method" do
+    let(:user_name_not_formated) { User.create(name: 'joe garcia', email: "joe@bloccit.com", password: "password") }
+    let(:user_name_formated) { User.create(name: 'Mike More', email: "mike@bloccit.com", password: "password") }
+
+    it "capitalizes the first and last name of a full name" do
+      expect(user_name_not_formated.name).to eq("Joe Garcia")
+      expect(user_name_formated.name).to eq("Mike More")
     end
   end
 
