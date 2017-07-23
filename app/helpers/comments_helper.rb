04ec311ee815d08d05b3e5d46c1701fe67cb10e7
Joseph <jvg_0119@yahoo.com>
2017-07-23 11:39:40 -0700

    assign-41-comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,7 +2,7 @@ module CommentsHelper
 
   def user_is_authorized_for_comment?(comment)
     #current_user == comment.user || current_user.admin?  if current_user # works OK
-    current_user && current_user == comment.user || current_user.admin?
+    current_user && (current_user == comment.user || current_user.admin?)
   end
 
 end
